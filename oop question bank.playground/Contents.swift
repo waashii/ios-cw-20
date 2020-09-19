fed17b874c1486409c171be3f9efadfb09565d07
Omar <omsi96@users.noreply.github.com>
2020-09-19 18:28:46 +0300

    first draft

---
+++ oop question bank.playground/Contents.swift
@@ -0,0 +1,54 @@
+
+/*: 1. Create a struct Person that includes
+- name
+- age
+- blood type
+ 
+ create 3 objects of that.
+ 
+ */
+
+
+/*: 2. Create a struct Rectangle that includes
+ - width
+ - height
+ and methods
+ + area
+ + permiter
+ + isSquare
+ 
+ */
+
+
+/*:
+ 3. Create a struct Circle that includes
+ - radius
+ and methods
+ + area
+ + permiter
+ */
+
+
+
+
+/*:
+ 4. Create a struct SuperHero that includes
+ - name
+ - super power
+ and methods
+ + superPower
+ */
+
+
+
+
+/*:
+ 5. Create a struct Pokemon that includes
+ - name
+ - level
+ - category
+ and methods
+ + attack
+ + defend
+ */
+
